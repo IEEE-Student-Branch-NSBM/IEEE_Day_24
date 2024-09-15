4846f32c71f1118ffa8cc6a7990deb49f11eb599
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2024-09-15 23:11:22 +0530

    feat(context-section): update content in context section component

--- src/components/sections/context-section.tsx
+++ src/components/sections/context-section.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const contextsection = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return <div></div>;
+};
 
-export default contextsection
+export default contextsection;
