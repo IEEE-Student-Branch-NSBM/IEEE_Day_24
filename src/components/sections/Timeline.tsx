53371eac2bdffb87d62463427d8f28302a6b2a5a
Anuja Rathnayaka <anujanisal19@gmail.com>
2024-09-16 23:49:52 +0530

    style: adjust padding and margins in resposive

--- src/components/sections/Timeline.tsx
+++ src/components/sections/Timeline.tsx
@@ -12,22 +12,24 @@ const Timeline = () => {
 			<h1 className="text-center text-3xl md:text-4xl font-semibold md:py-28 py-10">
 				TIMELINE
 			</h1>
+
 			<div className="max-w-7xl mx-auto w-full grid grid-cols-9 px-2 pb-10">
+				
 				{/* Stack 1 */}
 				<div className="col-span-4 w-full h-full md:my-5 my-0 md:block hidden">
 					<Image
 						className="mx-auto"
 						src={RegOpen}
 						alt="Register Open"
-						width={320}
-						height={200}
+						width={250}
+						height={250}
 					/>
 				</div>
 				<div className="relative col-span-1 w-full h-full flex justify-center items-center">
 					<div className="h-full w-1 bg-[#3DA6FF]"></div>
 					<div className="top-0 absolute w-6 h-6 rounded-full bg-[#3DA6FF] z-10 text-white text-center"></div>
 				</div>
-				<div className="md:col-span-4 w-full h-full md:my-5 my-0 col-span-8">
+				<div className="md:col-span-4 w-full h-full md:my-5 mb-5 col-span-8">
 					<TimelineTextContent
 						title="REGISTRATIONS OPEN"
 						description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -39,7 +41,7 @@ const Timeline = () => {
 					<div className="h-full w-1 bg-white"></div>
 					<div className="top-0 absolute w-6 h-6 rounded-full bg-[#3DA6FF] z-10 text-white text-center"></div>
 				</div>
-				<div className="md:col-span-4 w-full h-full md:my-5 my-0 col-span-8 flex justify-center items-center md:ml-24">
+				<div className="md:col-span-4 w-full h-full md:my-5 mb-5 col-span-8 flex justify-center items-center md:ml-24">
 					<TimelineTextContent
 						title="REGISTRATIONS CLOSE"
 						description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -49,7 +51,7 @@ const Timeline = () => {
 					<div className="h-full w-1 bg-white"></div>
 					<div className="top-0 absolute w-6 h-6 rounded-full bg-[#3DA6FF] z-10 text-white text-center"></div>
 				</div>
-				<div className="col-span-4 w-full h-full md:my-5 my-0 md:block hidden">
+				<div className="col-span-4 w-full h-full md:my-5 mb-5 md:block hidden">
 					<Image
 						src={RegClose}
 						alt="Register Closed"
@@ -59,7 +61,7 @@ const Timeline = () => {
 				</div>
 
 				{/* Stack 3 */}
-				<div className="col-span-4 w-full h-full md:my-5 my-0 md:block hidden">
+				<div className="col-span-4 w-full h-full md:mb-5  md:block hidden">
 					<Image
 						src={EventDay}
 						alt="IEEE Day Logo"
--- src/components/ui/timeline/TimelineTextContent.tsx
+++ src/components/ui/timeline/TimelineTextContent.tsx
@@ -8,8 +8,8 @@ const TimelineTextContent = ({
 	description,
 }: TimelineTextContentProps) => {
 	return (
-		<div className="w-full h-full rounded-md p-2 md:pl-4">
-			<h1 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium py-5">
+		<div className="w-full h-full rounded-md px-5 md:pl-4 md:py-4">
+			<h1 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium pb-3">
 				{title}
 			</h1>
 			<p className="text-gray-100 text-lg sm:text-xl pb-5 md:w-[80%] w-[100%]">
