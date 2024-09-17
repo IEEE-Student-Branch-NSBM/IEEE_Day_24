65729f342e056f6d7652a53d3d350d68ea0ce7a2
Anuja Rathnayaka <anujanisal19@gmail.com>
2024-09-17 16:02:50 +0530

    fix: improve mobile responsiveness for session section

--- src/components/ui/session/AgendaItem.tsx
+++ src/components/ui/session/AgendaItem.tsx
@@ -9,9 +9,9 @@ interface AgendaItemProps {
 
 const AgendaItem: React.FC<AgendaItemProps> = ({ phaseNumber, phaseTitle, time, description }) => {
   return (
-    <div className="flex flex-col md:flex-row items-center p-4 md:p-6 rounded-lg text-white my-5">
+    <div className="flex flex-col md:flex-row md:items-center p-4 md:p-6 rounded-lg text-white my-5">
       {/* Phase Number */}
-      <div className="flex flex-col items-center justify-center text-center p-5 border-b md:border-b-0 md:border-r border-gray-500 mb-4 md:mb-0 md:mr-5">
+      <div className="flex flex-col md:items-center justify-center md:px-5 p-1 border-b-2 md:border-b-0 md:border-r border-gray-500 pb-2 my-2 md:mb-0 md:mr-5">
         <div className="text-4xl font-sansation">{phaseNumber}</div>
         <div className="text-sm text-gray-400">phase</div>
       </div>
@@ -20,8 +20,8 @@ const AgendaItem: React.FC<AgendaItemProps> = ({ phaseNumber, phaseTitle, time, 
       <div className="flex-1">
         {/* Title and Time in the same row */}
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
-          <h2 className="text-[26px] md:text-2xl font-sansation">{phaseTitle}</h2>
-          <div className="text-[16px] font-medium mt-2 md:mt-0">{time}</div>
+          <h2 className="text-[28px] md:text-2xl font-sansation">{phaseTitle}</h2>
+          <div className="text-[18px] font-medium mt-2 md:mt-0">{time}</div>
         </div>
 
         {/* Description below */}
