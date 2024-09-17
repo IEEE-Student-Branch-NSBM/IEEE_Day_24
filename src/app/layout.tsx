a4fde4abeef19ac3a60dd80a1d02ff7fd41042e5
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2024-09-17 15:04:48 +0530

    refactor: add darked inner ieeeday logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<Navbar/>
+				<Navbar />
 				{children}
 			</body>
 		</html>
