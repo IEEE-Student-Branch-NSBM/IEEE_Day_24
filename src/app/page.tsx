e824e18f4e2ad36c5f37e292758567b947861039
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2024-09-19 15:03:56 +0530

    style: design side wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,24 @@ import Timeline from "@/components/sections/Timeline";
 import Session from "@/components/sections/Session";
 import Register from "@/components/sections/register";
 import RegisterSoon from "@/components/sections/register-soon";
+import Wrapper from "@/components/utility/wrapper";
 
 export default function Home() {
 	return (
 		<div>
 			<Countdown />
-			<Contextsection />
-			<Timeline />
-			<Session />
+			<Wrapper>
+				<Contextsection />
+			</Wrapper>
+			<Wrapper>
+				<Timeline />
+			</Wrapper>
+			<Wrapper>
+				<Session />
+			</Wrapper>
 			<RegisterSoon />
 			<Register />
+			
 		</div>
 	);
 }
---
+++ src/components/utility/wrapper.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface WrapperProps {
+  children: React.ReactNode;
+}
+
+const wrapper: React.FC<WrapperProps> = ({ children }) => {
+  return (
+    <div className="m-4 md:m-8 lg:m-16 xl:m-24">
+      {children}
+    </div>
+  );
+};
+
+export default wrapper;
