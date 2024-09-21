b461b85166ab1d9ad42d7a2e7ef5f273b45ea9b0
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2024-09-21 21:54:12 +0530

    refactor: do some modifications

--- src/components/ui/TimelineTextContent.tsx
+++ src/components/ui/TimelineTextContent.tsx
@@ -9,7 +9,7 @@ const TimelineTextContent = ({
 }: TimelineTextContentProps) => {
 	return (
 		<div className="w-full h-full rounded-md px-5 md:pl-4 md:pb-4">
-			<h1 className="font-sansation text-white text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-medium pb-3 md:pb-1">
+			<h1 className="font-sansation tracking-wider text-white text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-medium pb-3 md:pb-1">
 				{title}
 			</h1>
 			<p className="text-gray-100 text-lg sm:text-xl pb-5 md:w-[80%] w-[100%] md:mt-2">
